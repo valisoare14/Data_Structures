c1e7a41a07a3e06a8ef7935382d8eeb2bd97f19c
Soare Valentin <125280308+valisoare14@users.noreply.github.com>
2025-02-23 14:33:38 +0200

    Translate into english Source.c

--- listoflists/Source.c
+++ listoflists/Source.c
@@ -1,321 +1,405 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
 
-struct Angajat
+////////////////////////////////////////////////////////////////////////////////
+// Structures
+////////////////////////////////////////////////////////////////////////////////
+
+struct Employee
 {
-	char* nume;
-	unsigned int codDepartament;
-	int varsta;
-	float salariulOrar;
+	char* name;
+	unsigned int departmentCode;
+	int age;
+	float hourlySalary;
 };
-typedef struct Angajat AngajatInfo;
+typedef struct Employee EmployeeInfo;
 
 struct Node
 {
-	AngajatInfo* info;
+	EmployeeInfo* info;
 	struct Node* next;
 };
 typedef struct Node ListNode;
 
 struct ListOfList
 {
-	AngajatInfo* info;
+	EmployeeInfo* info;
 	struct ListOfList* next;
 	ListNode* neighbours;
 };
 typedef struct ListOfList DoubleListNode;
 
-//info
-AngajatInfo* createAngajat(char* , unsigned int , int , float );
-DoubleListNode* createDoubleListNode(AngajatInfo*);
-DoubleListNode* insertNode(DoubleListNode*,AngajatInfo*);
-ListNode* createNode(AngajatInfo*);
+////////////////////////////////////////////////////////////////////////////////
+// Function Prototypes
+////////////////////////////////////////////////////////////////////////////////
+
+EmployeeInfo* createEmployee(char*, unsigned int, int, float);
+DoubleListNode* createDoubleListNode(EmployeeInfo*);
+DoubleListNode* insertNode(DoubleListNode*, EmployeeInfo*);
+ListNode* createNode(EmployeeInfo*);
 void displayListOfLists(DoubleListNode*);
-void displayAngajat(AngajatInfo*);
-AngajatInfo** vectorAngajati(DoubleListNode*,int*,float);
-void displayVector(AngajatInfo**, int*);
-ListNode* determinareAngajatiDinDepartamentulX(DoubleListNode*, int);
-ListNode* getNode(ListNode**);
+void displayEmployee(EmployeeInfo*);
+EmployeeInfo** employeesAboveSalary(DoubleListNode*, int*, float);
+void displayArray(EmployeeInfo**, int*);
+ListNode* buildQueueFromDepartment(DoubleListNode*, int);
 void displayQueue(ListNode*);
-void golireQueue(ListNode**, float);
+ListNode* getNodeFromQueue(ListNode**);
+EmployeeInfo** smallestSalaries(ListNode*, int*);
+void clearQueueBySalary(ListNode**, float);
 
+////////////////////////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////////////////////////
 
-void main() {
+int main() 
+{
 	FILE* pFile = fopen("Data.txt", "r");
-	DoubleListNode* listaDeListe = NULL;
+	DoubleListNode* listOfLists = NULL;
+
 	if (pFile)
 	{
-		char nume[128];
-		unsigned int codDepartament;
-		int varsta;
-		float salariulOrar;
+		char name[128];
+		unsigned int departmentCode;
+		int age;
+		float hourlySalary;
 		char linebuffer[128];
 		char delimiter[] = ",\n";
 		char* token;
+
 		while (fgets(linebuffer, sizeof(linebuffer), pFile))
 		{
 			token = strtok(linebuffer, delimiter);
-			strcpy(nume, token);
+			strcpy(name, token);
+
 			token = strtok(NULL, delimiter);
-			codDepartament = atoi(token);
+			departmentCode = (unsigned int)atoi(token);
+
 			token = strtok(NULL, delimiter);
-			varsta = atoi(token);
+			age = atoi(token);
+
 			token = strtok(NULL, delimiter);
-			salariulOrar = atof(token);
-			AngajatInfo* ang = createAngajat(nume,codDepartament,varsta,salariulOrar);
-			listaDeListe = insertNode(listaDeListe,ang);
+			hourlySalary = (float)atof(token);
+
+			EmployeeInfo* emp = createEmployee(name, departmentCode, age, hourlySalary);
+			listOfLists = insertNode(listOfLists, emp);
 		}
-		//.1
-		displayListOfLists(listaDeListe);
+		fclose(pFile);
 
-		//.2
-		int* nrAngajati = (int*)malloc(sizeof(int));
-		nrAngajati[0] = 0;
-		AngajatInfo** angajati = vectorAngajati(listaDeListe, nrAngajati,160.5);
-		displayVector(angajati, nrAngajati);
+		// 1) Display the list-of-lists
+		displayListOfLists(listOfLists);
 
-		//.3
-		ListNode* coada = determinareAngajatiDinDepartamentulX(listaDeListe , 2);
-		//displayQueue(coada);
+		// 2) Create an array of employees whose salary is above 160.5
+		int* countAbove = (int*)malloc(sizeof(int));
+		countAbove[0] = 0;
+		EmployeeInfo** employeesArr = employeesAboveSalary(listOfLists, countAbove, 160.5f);
+		displayArray(employeesArr, countAbove);
 
-		//.4
-		int* nrAngajati1 = (int*)malloc(sizeof(int));
-		nrAngajati1[0] = 0;
-		AngajatInfo** angajati1 = vectorAngajatiSalariiMici(coada, nrAngajati1);
-		displayVector(angajati1, nrAngajati1);
-	}
-}
-AngajatInfo* createAngajat(char* nume, unsigned int codDepartament, int varsta, float salariulOrar)
-{
-	AngajatInfo* res = (AngajatInfo*)malloc(sizeof(AngajatInfo));
-	res->nume = (char*)malloc(strlen(nume) + 1);
-	strcpy(res->nume, nume);
-	res->codDepartament = codDepartament;
-	res->salariulOrar = salariulOrar;
-	res->varsta = varsta;
-	return res;
-}
-DoubleListNode* createDoubleListNode(AngajatInfo* ang)
-{
-	DoubleListNode* aux = (DoubleListNode*)malloc(sizeof(DoubleListNode));
-	aux->info = ang;
-	aux->neighbours = NULL;
-	aux->next = NULL;
-	return aux;
-}
-DoubleListNode* insertNode(DoubleListNode* listaDeListe,AngajatInfo* ang)
-{
-	if (listaDeListe == NULL)
-	{
-		listaDeListe = createDoubleListNode(ang);
-		return listaDeListe;
+		// 3) Build a queue from employees in department #2
+		ListNode* queue = buildQueueFromDepartment(listOfLists, 2);
+		// Optional: displayQueue(queue);
+
+		// 4) Create an array of employees with the 3 smallest salaries from queue
+		int* countSmallest = (int*)malloc(sizeof(int));
+		countSmallest[0] = 0;
+		EmployeeInfo** smallestSalariesArr = smallestSalaries(queue, countSmallest);
+		displayArray(smallestSalariesArr, countSmallest);
+
+		// Cleanup or further usage...
 	}
 	else
 	{
-		DoubleListNode* aux = listaDeListe;
-		while (listaDeListe != NULL && ang->codDepartament != listaDeListe->info->codDepartament)
+		printf("Could not open file: Data.txt\n");
+	}
+
+	return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Implementation
+////////////////////////////////////////////////////////////////////////////////
+
+// Creates a new Employee
+EmployeeInfo* createEmployee(char* name, unsigned int departmentCode, int age, float hourlySalary)
+{
+	EmployeeInfo* e = (EmployeeInfo*)malloc(sizeof(EmployeeInfo));
+	e->name = (char*)malloc(strlen(name) + 1);
+	strcpy(e->name, name);
+	e->departmentCode = departmentCode;
+	e->age = age;
+	e->hourlySalary = hourlySalary;
+	return e;
+}
+
+// Creates a new top-level node in the "ListOfList"
+DoubleListNode* createDoubleListNode(EmployeeInfo* emp)
+{
+	DoubleListNode* node = (DoubleListNode*)malloc(sizeof(DoubleListNode));
+	node->info = emp;
+	node->neighbours = NULL;
+	node->next = NULL;
+	return node;
+}
+
+// Inserts an Employee into the list-of-lists data structure
+DoubleListNode* insertNode(DoubleListNode* listOfLists, EmployeeInfo* emp)
+{
+	// If empty, simply create and return a new node
+	if (listOfLists == NULL)
+	{
+		listOfLists = createDoubleListNode(emp);
+		return listOfLists;
+	}
+	else
+	{
+		DoubleListNode* start = listOfLists;
+		// Search for a matching department
+		while (listOfLists != NULL && emp->departmentCode != listOfLists->info->departmentCode)
 		{
-			listaDeListe = listaDeListe->next;
+			listOfLists = listOfLists->next;
 		}
-		if (listaDeListe == NULL)
+
+		// If we reached the end (NULL), no matching department is found
+		if (listOfLists == NULL)
 		{
-			DoubleListNode* lista= createDoubleListNode(ang);
-			lista->next = aux;
-			aux = lista;
+			DoubleListNode* newHead = createDoubleListNode(emp);
+			newHead->next = start;
+			start = newHead;
 		}
 		else
 		{
-			ListNode* node = createNode(ang);
-			node->next = listaDeListe->neighbours;
-			listaDeListe->neighbours = node;
+			// We found the matching department node
+			ListNode* newNode = createNode(emp);
+			newNode->next = listOfLists->neighbours;
+			listOfLists->neighbours = newNode;
 		}
-		return aux;
-	}
-	
-}
-ListNode* createNode(AngajatInfo* ang)
-{
-	ListNode* aux = (ListNode*)malloc(sizeof(ListNode));
-	aux->info = ang;
-	aux->next = NULL;
-	return aux;
-}
-void displayListOfLists(DoubleListNode* listaDeListe)
-{
-	if (listaDeListe)
-	{
-		DoubleListNode* aux = listaDeListe;
-		do {
-			displayAngajat(aux->info);
-			if (aux->neighbours)
-			{
-				ListNode* axu = aux->neighbours;
-				do
-				{
-					displayAngajat(axu->info);
-					axu = axu->next;
-				} while (axu != NULL);
-			}
-			aux = aux->next;
-		} while (aux != NULL);
+		return start;
 	}
 }
-void displayAngajat(AngajatInfo* ang)
-{
-	if (ang)
-	{
-		printf("ID: %d->Nume: %s->Varsta: %d->Salariul: %.2f\n",ang->codDepartament,ang->nume,ang->varsta,ang->salariulOrar);
-	}
-}
-AngajatInfo** vectorAngajati(DoubleListNode* listaDeListe,int* nrAngajati , float prag)
-{
-	DoubleListNode* aux = listaDeListe;
-	while (aux)
-	{
-		if (aux->info->salariulOrar > prag)
-		{
-			nrAngajati[0]++;
-		}
-		if (aux->neighbours)
-		{
-			ListNode* axx = aux->neighbours;
-			while (axx)
-			{
-				if (axx->info->salariulOrar > prag)
-				{
-					nrAngajati[0]++;
-				}
-				axx = axx->next;
-			}
-		}
-		aux = aux->next;
-	}
-	AngajatInfo** vector = (AngajatInfo**)malloc(nrAngajati[0] * sizeof(AngajatInfo));
-	memset(vector, 0, nrAngajati[0] * sizeof(AngajatInfo));
-	aux = listaDeListe;
-	int currentIdx = 0;
-	while (aux)
-	{
-		if (aux->info->salariulOrar > prag)
-		{
-			vector[currentIdx] = createAngajat(aux->info->nume, aux->info->codDepartament, aux->info->varsta, aux->info->salariulOrar);
-			currentIdx++;
-		}
-		if (aux->neighbours)
-		{
-			ListNode* axx = aux->neighbours;
-			while (axx)
-			{
-				if (axx->info->salariulOrar > prag)
-				{
-					vector[currentIdx] = createAngajat(axx->info->nume, axx->info->codDepartament, axx->info->varsta, axx->info->salariulOrar);
-					currentIdx++;
-				}
-				axx = axx->next;
-			}
-		}
-		aux = aux->next;
-	}
-	return vector;
-}
-void displayVector(AngajatInfo** angajati, int* nrAngajati)
-{
-	printf("Vector:\n---------------\n");
-	for (int i = 0; i < nrAngajati[0]; i++)
-	{
-		displayAngajat(angajati[i]);
-	}
-}
-ListNode* determinareAngajatiDinDepartamentulX(DoubleListNode* listaDeListe, int departament)
-{
-	while (listaDeListe && listaDeListe->info->codDepartament!=departament)
-	{
-		listaDeListe = listaDeListe->next;
-	}
-	if (listaDeListe)
-	{
-		ListNode* coada=createNode(createAngajat(listaDeListe->info->nume, listaDeListe->info->codDepartament, listaDeListe->info->varsta, listaDeListe->info->salariulOrar));
-		coada->next = coada;
-		while (listaDeListe->neighbours)
-		{
 
-			ListNode* node= createNode(createAngajat(listaDeListe->neighbours->info->nume, listaDeListe->neighbours->info->codDepartament, listaDeListe->neighbours->info->varsta, listaDeListe->neighbours->info->salariulOrar));
-			node->next = coada->next;
-			coada->next = node;
-			coada = node;
-			listaDeListe->neighbours = listaDeListe->neighbours->next;
+// Creates a singly linked list node
+ListNode* createNode(EmployeeInfo* emp)
+{
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->info = emp;
+	node->next = NULL;
+	return node;
+}
+
+// Displays the entire list-of-lists
+void displayListOfLists(DoubleListNode* listOfLists)
+{
+	DoubleListNode* aux = listOfLists;
+	while (aux)
+	{
+		displayEmployee(aux->info);
+		if (aux->neighbours)
+		{
+			ListNode* n = aux->neighbours;
+			while (n)
+			{
+				displayEmployee(n->info);
+				n = n->next;
+			}
 		}
-		return coada;
+		aux = aux->next;
 	}
-	else
+}
+
+// Displays a single Employee
+void displayEmployee(EmployeeInfo* emp)
+{
+	if (emp)
+	{
+		printf("DeptID: %d | Name: %s | Age: %d | Salary: %.2f\n",
+		       emp->departmentCode, emp->name, emp->age, emp->hourlySalary);
+	}
+}
+
+// Creates an array of employees whose salary is above the given threshold
+EmployeeInfo** employeesAboveSalary(DoubleListNode* listOfLists, int* count, float threshold)
+{
+	DoubleListNode* aux = listOfLists;
+	// First pass: count how many match
+	while (aux)
+	{
+		if (aux->info->hourlySalary > threshold)
+		{
+			count[0]++;
+		}
+		ListNode* ln = aux->neighbours;
+		while (ln)
+		{
+			if (ln->info->hourlySalary > threshold)
+			{
+				count[0]++;
+			}
+			ln = ln->next;
+		}
+		aux = aux->next;
+	}
+
+	// Allocate the array
+	EmployeeInfo** result = (EmployeeInfo**)malloc(sizeof(EmployeeInfo*) * count[0]);
+	memset(result, 0, sizeof(EmployeeInfo*) * count[0]);
+
+	// Second pass: fill in the array
+	aux = listOfLists;
+	int idx = 0;
+	while (aux)
+	{
+		if (aux->info->hourlySalary > threshold)
+		{
+			result[idx++] = createEmployee(aux->info->name,
+			                               aux->info->departmentCode,
+			                               aux->info->age,
+			                               aux->info->hourlySalary);
+		}
+		ListNode* ln = aux->neighbours;
+		while (ln)
+		{
+			if (ln->info->hourlySalary > threshold)
+			{
+				result[idx++] = createEmployee(ln->info->name,
+				                               ln->info->departmentCode,
+				                               ln->info->age,
+				                               ln->info->hourlySalary);
+			}
+			ln = ln->next;
+		}
+		aux = aux->next;
+	}
+	return result;
+}
+
+// Displays an array of employees
+void displayArray(EmployeeInfo** arr, int* count)
+{
+	printf("Array of employees:\n-----------------\n");
+	for (int i = 0; i < count[0]; i++)
+	{
+		displayEmployee(arr[i]);
+	}
+}
+
+// Builds a queue (singly circular list) of employees from a given department
+ListNode* buildQueueFromDepartment(DoubleListNode* listOfLists, int dept)
+{
+	// Find the node in top-level list for that department
+	while (listOfLists && listOfLists->info->departmentCode != dept)
+	{
+		listOfLists = listOfLists->next;
+	}
+	if (listOfLists == NULL)
 	{
 		return NULL;
 	}
+
+	// Create a circular list from that node's info + neighbours
+	ListNode* queue = createNode(createEmployee(listOfLists->info->name,
+	                                           listOfLists->info->departmentCode,
+	                                           listOfLists->info->age,
+	                                           listOfLists->info->hourlySalary));
+	queue->next = queue; // circular
+	ListNode* tail = queue;
+
+	// Add each neighbour to that queue
+	ListNode* ln = listOfLists->neighbours;
+	while (ln)
+	{
+		ListNode* node = createNode(createEmployee(ln->info->name,
+		                                           ln->info->departmentCode,
+		                                           ln->info->age,
+		                                           ln->info->hourlySalary));
+		node->next = tail->next;
+		tail->next = node;
+		tail = node;
+		ln = ln->next;
+	}
+	return tail;
 }
-void displayQueue(ListNode* coada)
+
+// Displays and empties a queue
+void displayQueue(ListNode* tail)
 {
+	if (!tail) return;
+
 	ListNode* node = NULL;
-	printf("Coada:\n");
-	while ((node = getNode(&coada)) != NULL)
+	printf("Queue (circular list) content:\n");
+	while ((node = getNodeFromQueue(&tail)) != NULL)
 	{
-		displayAngajat(node->info);
+		displayEmployee(node->info);
+		// Optionally free node->info etc.
 	}
 }
-ListNode* getNode(ListNode** coada)
+
+// Gets one node from the front of the queue
+ListNode* getNodeFromQueue(ListNode** tail)
 {
-	if(*coada)
-	{
-		if ((*coada) == (*coada)->next)
-		{
-			ListNode* node = (*coada);
-			*coada = NULL;
-			return node;
-		}
-		else
-		{
-			ListNode* node = (*coada)->next;
-			(*coada)->next = (*coada)->next->next;
-			return node;
-		}
-	}
-	else
+	if (!(*tail)) 
 	{
 		return NULL;
 	}
+	ListNode* front = (*tail)->next;
+	if (front == (*tail))
+	{
+		// Only one element
+		*tail = NULL;
+		return front;
+	}
+	else
+	{
+		(*tail)->next = front->next;
+		return front;
+	}
 }
-vectorAngajatiSalariiMici(ListNode* coada,int* nrAngajati1)
+
+// Creates an array of the 3 smallest-salary employees from the given queue
+EmployeeInfo** smallestSalaries(ListNode* tail, int* count)
 {
-	nrAngajati1[0] = 3;
-	AngajatInfo** vector = (AngajatInfo**)malloc(nrAngajati1[0] * sizeof(AngajatInfo*));
-	memset(vector, 0, nrAngajati1[0] * sizeof(AngajatInfo*));
+	count[0] = 3; // as per the code snippet
+
+	EmployeeInfo** arr = (EmployeeInfo**)malloc(sizeof(EmployeeInfo*) * count[0]);
+	memset(arr, 0, sizeof(EmployeeInfo*) * count[0]);
+
 	for (int i = 0; i < 3; i++)
 	{
-		ListNode* minim = coada;
-		ListNode* aux = coada->next;
+		// Find min salary in the queue
+		ListNode* minNode = tail;
+		ListNode* aux = tail->next;
 		do
 		{
-			if (aux->info->salariulOrar < minim->info->salariulOrar)
+			if (aux->info->hourlySalary < minNode->info->hourlySalary)
 			{
-					minim = aux;
+				minNode = aux;
 			}
 			aux = aux->next;
-		} while (aux != coada);
-		vector[i] = createAngajat(minim->info->nume, minim->info->codDepartament, minim->info->varsta, minim->info->salariulOrar);
-		golireQueue(&coada, minim->info->salariulOrar);
+		} while (aux != tail);
+
+		// Copy that employee
+		arr[i] = createEmployee(minNode->info->name,
+		                        minNode->info->departmentCode,
+		                        minNode->info->age,
+		                        minNode->info->hourlySalary);
+
+		// Mark or set the min salary employee
+		clearQueueBySalary(&tail, minNode->info->hourlySalary);
 	}
-	return vector;
+	return arr;
 }
-void golireQueue(ListNode** coada, float salariul)
+
+// Recursively marks the queue node with the specified salary
+// with some sentinel value (like 1000), as done in code snippet
+void clearQueueBySalary(ListNode** tail, float salary)
 {
-	if ((*coada)->info->salariulOrar != salariul)
+	if ((*tail)->info->hourlySalary != salary)
 	{
-		golireQueue(&(*coada)->next, salariul);
+		clearQueueBySalary(&(*tail)->next, salary);
 	}
 	else
 	{
-		(*coada)->info->salariulOrar = 1000;
+		(*tail)->info->hourlySalary = 1000; 
 	}
 }
